f3585995078656c4b6977f9fb31d36563e4169ac
Ruby <rubykulikowska@gmail.com>
2019-06-22 16:27:50 -0400

    start on the about section

--- wolfs/src/components/About.js
+++ wolfs/src/components/About.js
@@ -4,7 +4,29 @@ class About extends Component {
   render() {
       return (
         <div id="about">
-            Coming Soon!
+            <h1> A Short History of the Reintroduction</h1>
+            The gray wolf was purposefully shot, trapped, and poisoned in Yellowstone National Park since the park&apos;s establishment
+            in 1872, until the wolf&apos;s complete extirpation in the mid 1930s. In the absence of one its major predators, the Yellowstone ecosystem 
+            began to decline. With the overpopulation of large game animals, chiefly elk and deer, much of the vegetation in the park
+            became drastically overgrazed, leading to the decline in populations of many trees, birds, fish, and small game.
+            <br /><br />
+
+            Officially endangered since 1967, wildlife biologists had been calling for a restoration of wolves into the northern 
+            rockies since the 60s, and with the passage of the Endangered Species Act in 1973, the US Fish and Wildlife Service 
+            was obligated to develop a plan. The year before saw the first official meeting to discuss the reintroduction into Yellowstone 
+            specifically, and in 1980 a plan was approved. The objective for the program was to restore a consistent population of at 
+            least 10 packs that have produced pups for 3 consecutive years, at which point the gray wolf would be removed from the
+            endangered species list of the northern rocky mountains. 
+            <br /><br />
+
+            In January 1995, after 20 years of planning, fourteen wolves were captured in Hinston, Alberta, just east of Jasper
+            National Park. Journeying 880km south to Yellowstone, they were then released into 3 acclimation pens, and after 10 weeks 
+            released into the wild. The following year saw seventeen wolves from an area east of Willingston Lake, British Columbia (approx 1200km from Yellowstone)
+            translocated into 4 acclimatation pens and later released. The wolves did exceptionally well, exceeding population growth expectations, even breeding in pens. All but one pack 
+            remained in the park, with a 1995 total of 21 wolves, and with 3 litters born in 1996 boosting the population  to 51.
+            <br /><br />
+
+            <h1>Since the Reintroduction</h1>
         </div>
       );
   }
